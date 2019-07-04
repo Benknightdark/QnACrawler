aa5f0519a3ac657fefbb88b1e3ab56458293faa5
Ben Kung <ben_kung@infofab.com>
2019-07-04 11:21:31 +0800

    哏

--- src/firstSite.ts
+++ src/firstSite.ts
@@ -1,25 +1,61 @@
+//秀傳醫療體系
 import * as puppeteer from 'puppeteer'
 import * as cheerio from "cheerio"
+const $ = cheerio
+
+const getTrArray = async (tr: Cheerio) => {
+    let trArray = [];
+    for (let index = 1; index < tr.length - 2; index++) {
+        const element = tr[index];
+        const td = $(element).find('td');
+        let detailID = ''
+        //let detailUrl=`http://www.show.org.tw/QA_Detail.aspx?Kind=5&Dept=TT&No=${detailID}`;
+        if (Number.parseInt($(td[3]).text()) !== 0) {
+            detailID = $($(td[0]).find('input')[1]).val().trim();
+            trArray.push({
+                question: $(td[0]).text().trim(),
+                url: `http://www.show.org.tw/QA_Detail.aspx?Kind=5&Dept=TT&No=${detailID}`
+            })
+        }
+
+    }
+    // console.log(trArray)
+    return trArray;
+}
 export const firstsite = (async () => {
-    const browser = await puppeteer.launch({ headless: true });
+    const browser = await puppeteer.launch({ headless: false });
     const page = await browser.newPage();
     await page.goto('http://www.show.org.tw/QA_List.aspx?Kind=5&Dept=TT');
-    //   await page.screenshot({path: 'example.png'});
     const html = await page.content()
-    const $ = cheerio.load(html);
-    
-  //  const title = $('title').text();
- //   console.log(title)
-   // await browser.close();
-    // await page.evaluate(() => {
-    //     // let imageElements: any = document.querySelectorAll('img')
-    //     // imageElements = [...imageElements]
-    //     // // imageElements = imageElements.filter(img => img.getAttribute('sizes') === '293px')
-    //     // let links = imageElements.map(img => img.getAttribute('src'))
-    //     const doc=$(document);
-    //     console.log(document)
+    const $2 = cheerio.load(html);
+    const table = $2('.LoanTable');
 
-    //   console.log(  doc.find(`#LoanTable`).length)
-        
+
+
+    //get tr
+    const tr = table.find('tr');
+    const nTRArray = await getTrArray(tr)
+    console.log(nTRArray)
+
+
+    await page.evaluate( () => {    
+        const targetElement: any = document.querySelector('#ctl00_ContentPlaceHolder1_GV_GuestList > tbody > tr:nth-child(12) > td > table > tbody > tr > td:nth-child(2) > a')
+        targetElement&& targetElement.click();
+       // console.log(document)
+    })
+    setTimeout(async () => {
+        const html = await page.content()
+        const $2 = cheerio.load(html);
+        const table = $2('.LoanTable');
+        //get tr
+        const tr = table.find('tr');
+        const nTRArray = await getTrArray(tr)
+        console.log(nTRArray)
+    }, 3000);
+
+    
+
+
+
     // });
 })();
