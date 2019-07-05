8de10d10992e2e80e4dfc5302bdb0d999fc2a88c
Ben Kung <ben_kung@infofab.com>
2019-07-05 11:01:44 +0800

    哏

--- src/Helpers/mergFileHelper.ts
+++ src/Helpers/mergFileHelper.ts
@@ -1,22 +1,43 @@
 import * as mergeFiles from 'merge-files';
 import * as fs from 'fs'
+import * as tsv from 'tsv'
+import { rejects } from 'assert';
+//var tsv is the TSV file with headers
+
+  const mergFile=async(files,testFolder)=>{
+      let data=  files.map(async file=>{
+            let content=await   fs.readFileSync(testFolder+"/"+file, 'utf8');
+         //   let json=await tsvJSON(content);
+            // array=await [...array, ...JSON.parse(json)];     
+          return content
+
+        });
+        let nData=await Promise.all(data)
+
+        return nData;
+    
+  }
 (async()=>{
     let testFolder = '../Data';
     const outPutPath =__dirname + '/result.tsv';;
     let inputPathList =[];
     fs.readdir(testFolder, async (err, files) => {
-        
-      files.forEach(file => {
-        console.log(file);
-        inputPathList.push(testFolder+"/"+file)
-      });
-      console.log(inputPathList)
-      const status = await mergeFiles(inputPathList, outPutPath);
-      // or
-      mergeFiles(inputPathList, outPutPath).then((status) => {
-          // next
-          console.log(status)
-      });
+        const bb=await mergFile(files,testFolder)
+     let nbb=   bb.map((b:any)=>{
+         let c=b.replace(`question\tanswer\r\n`)
+         return c
+      }).join(",")
+
+      //  await fs.writeFileSync(`../Data/result.tsv`, ndata);
+       await fs.writeFileSync(`../Data/result.tsv`, new Buffer(nbb),{encoding:'utf8'});
+
+    //   console.log(inputPathList)
+    //   const status = await mergeFiles(inputPathList, outPutPath);
+    //   // or
+    //   mergeFiles(inputPathList, outPutPath).then((status) => {
+    //       // next
+    //       console.log(status)
+    //   });
     });
 
 })()
