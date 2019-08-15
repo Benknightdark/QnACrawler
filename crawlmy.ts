7187852775609a162756725e1c812de086a36cdf
BenKung <moon791017@gmail.com>
2019-08-15 21:26:44 +0800

    ok

--- crawlmy.ts
+++ crawlmy.ts
@@ -1,5 +1,23 @@
 import { ClientConfig, crawlClient } from './src/Helpers/crawlHelper';
 let mainUrl=`http://163.32.129.93/khjh/11/`;
+let folder=`doc`
+var fs = require('fs');;
+const getContent=async (url)=>{
+    let clientConfig2: ClientConfig = new ClientConfig();
+    clientConfig2.ignoreStatic = true;
+    clientConfig2.url = url;
+    const { browser, page, $ } = await crawlClient(clientConfig2);
+    console.log()
+   // console.log($('body').text())
+    
+let fileName=$('body').find('span').first().text();
+fs.writeFile(`./${folder}/${fileName}.txt`, $('body').text(), (err) => {
+  if (err) throw err;
+  console.log(`The ${fileName} has been saved!`);
+  browser.close();
+});
+
+}
 (async()=>{
     let clientConfig: ClientConfig = new ClientConfig();
     clientConfig.ignoreStatic = true;
@@ -16,6 +34,15 @@ let mainUrl=`http://163.32.129.93/khjh/11/`;
         
     }
     var filterUrls=urlAray.splice(0,10);
-    urlAray=  urlAray.filter(a=>filterUrls.filter(b=>b==a).length==0);
+    urlAray=  urlAray.filter(a=>filterUrls.filter(b=>b==a).length==0)
+    .map(c=>`${mainUrl}${c}`);
+   // console.log(urlAray[0])
+for (let index = 0; index < urlAray.length; index++) {
+    const element = urlAray[index];
+    await getContent(element)
+}
+
+
+
     
 })()
